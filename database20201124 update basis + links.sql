0d20e802a5b5c52da72d3c92fc918e9858964d13
yama24 <maulana24@live.com>
2020-11-24 10:57:14 +0700

    Update database20201124 update basis + links.sql

--- database20201124 update basis + links.sql
+++ database20201124 update basis + links.sql
@@ -11,41 +11,18 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
--- membuang struktur untuk table internusadata.basis
-CREATE TABLE IF NOT EXISTS `basis` (
-  `basis_id` int(250) NOT NULL AUTO_INCREMENT,
-  `basis_nama` text NOT NULL,
-  `basis_ttl` date NOT NULL,
-  `basis_tempat_kerja` text NOT NULL,
-  `basis_gender` text NOT NULL,
-  `basis_pekerjaan` text NOT NULL,
-  `basis_provinsi` text NOT NULL,
-  `basis_kabupaten` text NOT NULL,
-  `basis_kecamatan` text NOT NULL,
-  `basis_desa` text NOT NULL,
-  `basis_phone` double NOT NULL,
-  `basis_email` text NOT NULL,
-  `tipe_grup` text NOT NULL,
-  `grup` text NOT NULL,
-  `slug` text NOT NULL,
-  `basis_datainput` datetime NOT NULL,
-  PRIMARY KEY (`basis_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC;
 
--- Pengeluaran data tidak dipilih.
+ALTER TABLE basis
+ADD COLUMN tipe_grup TEXT NOT NULL AFTER basis_email,
+ADD COLUMN grup TEXT NOT NULL AFTER tipe_grup,
+ADD COLUMN slug TEXT NOT NULL AFTER grup;
 
--- membuang struktur untuk table internusadata.links
-CREATE TABLE IF NOT EXISTS `links` (
-  `links_id` int(11) NOT NULL AUTO_INCREMENT,
-  `links_tipe` text NOT NULL,
-  `links_nama` text NOT NULL,
-  `links` text NOT NULL,
-  `links_slug` text NOT NULL,
-  `tipe` int(11) NOT NULL,
-  PRIMARY KEY (`links_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC;
 
--- Pengeluaran data tidak dipilih.
+
+ALTER TABLE links
+ADD COLUMN links_slug TEXT NOT NULL AFTER links,
+ADD COLUMN tipe INT(11) NOT NULL AFTER links_slug ;
+
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
